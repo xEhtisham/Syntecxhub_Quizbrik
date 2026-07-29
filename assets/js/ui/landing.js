8941fc084980978c245d0acfc5c3e4c5c8a7bd42
xEhtisham <ehtishamkhan013@gmail.com>
2026-07-29 19:04:15 +0500

    edited landing page

--- assets/js/ui/landing.js
+++ assets/js/ui/landing.js
@@ -14,17 +14,92 @@
  */
 export function renderLandingPage() {
   return `
-    
-        <section class="landing">
 
-            <h1>Quizbrik</h1>
+        <main class="landing">
 
-            <p>
-                Test Your Knowledge.
-                Learn. Compete. Improve.
-            </p>
+            <section class="card">
 
-        </section>
+                <header>
+
+                    <h1>Quizbrik</h1>
+
+                    <p>
+                        Test Your Knowledge.
+                        Learn. Compete. Improve.
+                    </p>
+
+                </header>
+
+                <section>
+
+                    <div>
+
+                        <label for="category">
+                            Category
+                        </label>
+
+                        <select
+                            id="category"
+                            class="input"
+                        >
+                            <option>Science</option>
+                            <option>History</option>
+                            <option>Technology</option>
+                        </select>
+
+                    </div>
+
+                    <br>
+
+                    <div>
+
+                        <label for="difficulty">
+                            Difficulty
+                        </label>
+
+                        <select
+                            id="difficulty"
+                            class="input"
+                        >
+                            <option>Easy</option>
+                            <option selected>Medium</option>
+                            <option>Hard</option>
+                        </select>
+
+                    </div>
+
+                    <br>
+
+                    <div>
+
+                        <label for="questions">
+                            Number of Questions
+                        </label>
+
+                        <input
+                            id="questions"
+                            class="input"
+                            type="number"
+                            value="10"
+                            min="5"
+                            max="50"
+                        >
+
+                    </div>
+
+                </section>
+
+                <br>
+
+                <button
+                    class="btn"
+                >
+                    Start Quiz
+                </button>
+
+            </section>
+
+        </main>
 
     `;
 }
